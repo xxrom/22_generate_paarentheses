833b9b6ebcf2e453f79562ca42422e37a01f26dd
Chernyshov_Nikita <xpom5566@mail.ru>
2020-03-31 23:46:38 +0300

    feat: solved 89%/ 6.5% , using strings

--- main.py
+++ main.py
@@ -1,24 +1,77 @@
+from typing import List
+
+
+class Solution:
+
+  def generate(self, currentPath: str, open: str, close: str,
+               ans: List[str]) -> List[str]:
+    if len(open) == 0 and len(close) == 0:
+      return ans + [str(currentPath)]
+
+    ans1 = []
+    ans2 = []
+    if len(open) >= len(close):
+      ans1 = self.generate(currentPath + '(', open[1:], close[:], ans)
+    if len(open) < len(close):
+      if len(open) > 0:
+        ans1 = self.generate(currentPath + '(', open[1:], close[:], ans)
+      if len(close) > 0:
+        ans2 = self.generate(currentPath + ')', open[:], close[1:], ans)
+
+    return ans1 + ans2
+
+  def generateParenthesis(self, n: int) -> List[str]:
+    open = '(' * n
+    close = ')' * n
+
+    return self.generate('', open, close, [])
+
+
 my = Solution()
-v = 0
-ans = my.(v)
+n = 7
+ans = my.generateParenthesis(n)
 print("ans", ans)
 
-n = 3
+# n = 3
 
-(((
+# (((
 
-0 -> 1 -> 2
-0 <- 1 <- 2
-((()))
+# 0 -> 1 -> 2
+# 0 <- 1 <- 2
+# ((()))
 
-0 -> 1
-0 <- 1
-(())
-0 -> 2
-0 <- 2
-(()())
+# 0 -> 1
+# 0 <- 1
+# (())
+# 0 -> 2
+# 0 <- 2
+# (()())
 
-0 -> 1
-0 <- 1
+# 0 -> 1
+# 0 <- 1
 
-(())
+# (())
+
+# ((()))
+# 111000
+# (()())
+# 110100
+# (())()
+# 110010
+
+# ((
+# 2,0
+# (())
+# 2,2
+
+# SHOULD WORK =))))
+# (((  )))  path = ''
+#   ((  )))  path = '('
+#     ((  ))  path = '()'
+#     (  )))  path = '(('
+#       _  )))  path = '((('
+#       (  ))   path = '(()'
+#         _  )))  path = '(()('
+#         (  )    path = '(())'
+#           _  )  path = '(())('
+#             _  _  path = '(())()'
